86a174617dfdac3026b3c69d7ed868ff85c65c14
A-fethi <fethiabderrahmane1@gmail.com>
2025-08-25 14:02:39 +0100

    new parser with echo command

--- src/parser.rs
+++ src/parser.rs
@@ -20,13 +20,78 @@ impl CommandParser {
             return Ok(None);
         }
 
-        let parts: Vec<&str> = trimmed.split_whitespace().collect();
+        let mut parts = Vec::new();
+        let mut current_part = String::new();
+        let mut in_quotes = false;
+        let mut quote_char = '\0';
+        let mut chars = trimmed.chars();
+
+        while let Some(ch) = chars.next() {
+            match ch {
+                '"' | '\'' => {
+                    if !in_quotes {
+                        in_quotes = true;
+                        quote_char = ch;
+                    } else if ch == quote_char {
+                        in_quotes = false;
+                        quote_char = '\0';
+                    } else {
+                        current_part.push(ch);
+                    }
+                }
+                '\\' => {
+                    if in_quotes {
+                        if let Some(next_ch) = chars.next() {
+                            match next_ch {
+                                'n' => current_part.push('\n'),
+                                't' => current_part.push('\t'),
+                                'r' => current_part.push('\r'),
+                                '\\' | '"' | '\'' => current_part.push(next_ch),
+                                _ => {
+                                    current_part.push('\\');
+                                    current_part.push(next_ch);
+                                }
+                            }
+                        } else {
+                            current_part.push('\\');
+                        }
+                    } else {
+                        current_part.push(ch);
+                    }
+                }
+                ' ' | '\t' => {
+                    if !in_quotes {
+                        if !current_part.is_empty() {
+                            parts.push(current_part.clone());
+                            current_part.clear();
+                        }
+                    } else {
+                        current_part.push(ch);
+                    }
+                }
+                _ => {
+                    current_part.push(ch);
+                }
+            }
+        }
+
+        if in_quotes {
+            return Err(ShellError::ParseError(format!(
+                "Unclosed quote: expected closing '{}'",
+                quote_char
+            )));
+        }
+
+        if !current_part.is_empty() {
+            parts.push(current_part);
+        }
+
         if parts.is_empty() {
             return Ok(None);
         }
 
-        let name = parts[0].to_string();
-        let args = parts[1..].iter().map(|s| s.to_string()).collect();
+        let name = parts[0].clone();
+        let args = parts[1..].to_vec();
 
         Ok(Some(Command { name, args }))
     }
